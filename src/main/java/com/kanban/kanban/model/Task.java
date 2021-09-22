9b273fdfa1439378a22627563edfbef1fa451227
Rafaela de Jesus Alves Candido <CIANDT\rcandido@lnb027960cps.ciandt.global>
2021-09-22 16:09:07 -0300

    criação de data sql

--- src/main/java/com/kanban/kanban/model/Task.java
+++ src/main/java/com/kanban/kanban/model/Task.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,6 +26,7 @@ public class Task {
     private Long id;
     private String title;
     private LocalDate date;
+    @Enumerated(EnumType.STRING)
     private Status status;
     private String color;
     @ManyToOne
